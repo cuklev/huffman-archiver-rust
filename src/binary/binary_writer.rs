e73247d3df23d9935bed1a0763de20ae2f8a1804
Kristiyan Tsaklev <cuki@cukii.tk>
2017-02-03 23:02:14 +0200

    Add more more functionality

--- src/binary/binary_writer.rs
+++ src/binary/binary_writer.rs
@@ -15,7 +15,7 @@ impl<T: Write> BinaryWriter<T> {
         }
     }
 
-    pub fn put(&mut self, x: u8) {
+    pub fn put_bit(&mut self, x: u8) {
         self.index -= 1;
         self.buffer |= x << self.index;
         if self.index == 0 {
@@ -23,6 +23,21 @@ impl<T: Write> BinaryWriter<T> {
             self.index = 8;
             self.buffer = 0;
         }
+    }
+
+    pub fn put_u8(&mut self, x: u8) {
+        self.writer.write(&[self.buffer | (x >> (8 - self.index))]);
+        self.buffer = x << self.index;
+    }
+
+    pub fn put_u64(&mut self, x: u64) {
+        self.writer.write(&[self.buffer | (x >> (64 - self.index)) as u8]);
+        let mut i = 56 - self.index;
+        while i >= 0 {
+            self.writer.write(&[(x >> i) as u8]);
+            i -= 8;
+        }
+        self.buffer = (x << self.index) as u8;
     }
 }
 
